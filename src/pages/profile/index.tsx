a04293046d43f9c6d02a971ce097125a8a8715fa
Kayky Lima <kayky.limaro@gmail.com>
2023-10-30 11:07:33 -0300

    #

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,22 +1,18 @@
-import Head from "next/head"
-import styles from "./styles.module.scss"
+import Head from "next/head";
+import styles from "./styles.module.scss";
 import Image from "next/image";
 import cameraAdd from "../../../public/cameraAdd.svg";
 
 export default function Profile() {
-    return(
-        <>
-        <Head>
-            <title>Perfil | Ischedule</title>
-        </Head>
-            <div className={styles.containerCenter}>
-                <div className={styles.containerTop}>
-                    <Image src={cameraAdd} alt="camera add icon" width={20} />
-                    
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Perfil | Ischedule</title>
+      </Head>
+      <div className={styles.containerCenter}>
+      </div>
+    </>
+  );
 }
 
 // export const getServerSideProps = canSSRAuth(async (ctx) => {
