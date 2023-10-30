f3f1625b35fa8e1b9e10504a824fb51dc6c6d6fd
Kayky Lima <kayky.limaro@gmail.com>
2023-10-30 10:13:32 -0300

    style changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,64 +1,72 @@
-import { useEffect } from  'react';
-import Link from 'next/link'
-import styles from './styles.module.scss'
-import Image from 'next/image'
-import logoImg from '../../../public/logoImg.svg'
-import { useState } from "react"
-import { useRouter } from 'next/router'
+import { useEffect } from "react";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import logoImg from "../../../public/logoImg.svg";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 function Header() {
-    const [focus, setFocus] = useState('dashboard')
-    const router = useRouter()
-    
-    useEffect(() => {
-      router.push('/dashboard')
-    }, [])
-    
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
+  const [focus, setFocus] = useState("dashboard");
+  const router = useRouter();
 
-                <Link href="/dashboard">
-                    <Image src={logoImg} alt='logo ishedule' />
-                </Link>
+  useEffect(() => {
+    router.push("/dashboard");
+  }, []);
 
-                <nav className={styles.navMenu}>
-                    <button onClick={() => {
-                        router.push('/dashboard')
-                        setFocus('dashboard')
-                    }}>
-                        <p className={focus === 'dashboard' && styles.focus}>Tela inicial</p>
-                    </button>
-                    <button onClick={() => {
-                        router.push('/schedule')
-                        setFocus('schedule')
-                    }}>
-                        <p className={focus === 'schedule' && styles.focus}>Agenda</p>
-                    </button>
-                    <button onClick={() => {
-                        router.push('/workers')
-                        setFocus('workers')
-                    }}>
-                        <p className={focus === 'workers' && styles.focus}>Profissionais</p>
-                    </button>
-                    <button onClick={() => {
-                        router.push('/profile')
-                        setFocus('profile')
-                    }}>
-                        <p className={focus === 'profile' && styles.focus}>Editar perfil</p>
-                    </button>
-                </nav>
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href="/dashboard">
+          <Image src={logoImg} alt="logo ishedule" />
+        </Link>
 
-            </div>
-        </header>
-    )
+        <nav className={styles.navMenu}>
+          <button
+            onClick={async () => {
+              await router.push("/dashboard");
+              setFocus("dashboard");
+            }}
+          >
+            <p className={focus === "dashboard" && styles.focus}>
+              Tela inicial
+            </p>
+          </button>
+          <button
+            onClick={async () => {
+              await router.push("/schedule");
+              setFocus("schedule");
+            }}
+          >
+            <p className={focus === "schedule" && styles.focus}>Agenda</p>
+          </button>
+          <button
+            onClick={async () => {
+              await router.push("/workers");
+              setFocus("workers");
+            }}
+          >
+            <p className={focus === "workers" && styles.focus}>Profissionais</p>
+          </button>
+          <button
+            onClick={async () => {
+              await router.push("/profile");
+              setFocus("profile");
+            }}
+          >
+            <p className={focus === "profile" && styles.focus}>Editar perfil</p>
+          </button>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export function LayoutHeader({children}) {
-    return(
-        <>
-            <Header/>
-            {children}
-        </>
-    )
+export function LayoutHeader({ children }) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
 }
--- src/pages/workers/index.tsx
+++ src/pages/workers/index.tsx
@@ -1,5 +1,10 @@
 import Head from "next/head"
 import styles from "./styles.module.scss"
+import Image from "next/image";
+import plus from "../../../public/plus.svg";
+import dtWorker from "../../../public/dtWorker.svg";
+import pencil from "../../../public/pencil.svg";
+import trash from "../../../public/trash.svg";
 
 export default function Workers() {
     return (
@@ -8,7 +13,13 @@ export default function Workers() {
                 <title>Profissionais | Ischedule</title>
             </Head>
             <div className={styles.containerCenter}>
-                workers
+                <div className={styles.containerTop}>
+                    <p>Membros</p>
+                    <button className={styles.btnNew}>
+                        Novo 
+                        <Image src={plus} alt="plus" width={20} />
+                    </button>
+                </div>
             </div>
         </>
     )
