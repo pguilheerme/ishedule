fb56d8df90d14a2bac78de21a3f80019faa52544
Pedro Guilherme <pedroguinasc2002@gmail.com>
2023-11-21 14:16:57 -0300

    modal service fixed

--- src/components/ModalService/index.tsx
+++ src/components/ModalService/index.tsx
@@ -7,8 +7,9 @@ import Image from 'next/image'
 import pencil from '../../../public/pencilWhite.svg'
 import cameraAdd from '../../../public/cameraAdd.svg'
 import { InputNumber } from 'primereact/inputnumber';
-
-
+import { firebase } from '../../services/firebase'
+import { getDownloadURL } from 'firebase/storage'
+import { api } from '@/services/apiClient'
 
 
 type propsModalService = {
@@ -29,11 +30,11 @@ export function ModalService({ open, onClose, edit = false, service }: propsModa
     const [serviceName, setServiceName] = useState<string>()
     const [servicePrice, setServicePrice] = useState<number>()
     const [serviceTime, setServiceTime] = useState<string>()
-    const [imageAvatar, setImageAvatar] = useState(null)
-    const [bannerUrl, setImageUrl] = useState('')
+    const [serviceAvatar, setServiceAvatar] = useState(null)
+    const [serviceUrl, setServiceUrl] = useState('')
     const [disabled, setDisabled] = useState(false);
 
-    function handleBannerFile(e) {
+    function handleServiceFile(e) {
         if (!e.target.files) {
             return;
         }
@@ -45,8 +46,60 @@ export function ModalService({ open, onClose, edit = false, service }: propsModa
         }
 
         if (image.type === "image/jpeg" || image.type === "image/png") {
-            setImageAvatar(image);
-            setImageUrl(URL.createObjectURL(e.target.files[0]));
+            setServiceAvatar(image);
+            setServiceUrl(URL.createObjectURL(e.target.files[0]));
+        }
+    }
+
+    async function uploadServiceAvatar(image: File) {
+
+        try {
+            const randomId = Math.floor(Math.random() * 99999999 + 1);
+
+            const storageRef = firebase.storage().ref().child(`serviceAvatar/${randomId}-${image.name}`)
+            const snapshot = await storageRef.put(image)
+
+            const url = await getDownloadURL(snapshot.ref)
+            return url
+
+        } catch (error) {
+            console.log(error)
+            return false
+        }
+    }
+
+    const handleCreateService = async (e) => {
+        setDisabled(true)
+        e.preventDefault()
+        try {
+            
+            const serviceUrl = await uploadServiceAvatar(serviceAvatar)
+
+            const response = await api.post('/services-company', {
+                name: serviceName,
+                price: String(servicePrice),
+                background_img_url: serviceUrl,
+                estimated_time: serviceTime
+            }, {
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            })
+
+            console.log(servicePrice)
+
+            console.log(response);
+            
+
+            getDataCompany()
+            setServiceUrl('')
+            setServiceAvatar(null)
+            setServiceName('')
+            setServicePrice(null)
+            onClose()
+
+        } catch (error) {
+            console.log(error)
         }
     }
 
@@ -60,34 +113,34 @@ export function ModalService({ open, onClose, edit = false, service }: propsModa
         >
             <div className={styles.containerCenter}>
                 <div className={styles.containerHeader}>
-                    <p>{edit ? 'Editar' : 'Adicionar novo'} membro</p>
+                    <p>{edit ? 'Editar' : 'Adicionar novo'} serviço</p>
                 </div>
                 <div className={styles.containerForm}>
                     <form>
-                        <div className={bannerUrl ? styles.edit : styles.labelAvatar}>
-                            <label htmlFor="inpAvatar">
-                                <Image src={bannerUrl ? pencil : cameraAdd} alt="Camera add icon" width={20} className={styles.image} />
+                        <div className={serviceUrl ? styles.editBanner : styles.labelBanner}>
+                            <label htmlFor="inpServiceAvatar">
+                                <Image src={serviceUrl ? pencil : cameraAdd} alt="Camera add icon" width={20} className={styles.image} />
                             </label>
                             <input
                                 required
                                 type="file"
-                                id="inpAvatar"
+                                id="inpServiceAvatar"
                                 accept="image/png, image/jpeg"
-                                onChange={handleBannerFile}
+                                onChange={handleServiceFile}
                             />
 
-                            {bannerUrl && (
+                            {serviceUrl && (
                                 <Image
-                                    src={bannerUrl}
-                                    alt="Foto de perfil"
+                                    src={serviceUrl}
+                                    alt="Foto do serviço"
                                     width={50}
                                     height={50}
-                                    className={styles.avatarPreview}
+                                    className={styles.bannerPreview}
                                 />
                             )}
                         </div>
 
-                        <label>Nome do serviço</label>
+                        <label className={styles.labelName}>Nome do serviço</label>
                         <div className={styles.allInput}>
                             <input
                                 required
@@ -97,29 +150,31 @@ export function ModalService({ open, onClose, edit = false, service }: propsModa
                                 className={styles.input}
                             />
                         </div>
-                        <label>Preço</label>
-                        <div className={styles.allInput}>
-                            <InputNumber
-                                inputId='currency-brazil'
-                                value={servicePrice}
-                                onChange={(e) => setServicePrice(e.value)}
-                                mode="currency"
-                                currency='BRL'
-                                locale='pt-BR'
-                                className={styles.input}
-                            />
-                        </div>
-                        <label>Tempo estimado</label>
-                        <div className={styles.allInput}>
-                                <input
-                                value={serviceTime}
-                                onChange={(e) => setServiceTime(e.target.value)} 
-                                type="time" 
+                        <div className={styles.otherInputs}>
+                            <div className={styles.price_input}>
+                                <label>Preço</label>
+                                <InputNumber
+                                    inputId='currency-brazil'
+                                    value={servicePrice}
+                                    onChange={(e) => setServicePrice(e.value)}
+                                    mode="currency"
+                                    currency='BRL'
+                                    locale='pt-BR'
                                 />
+                            </div>
+                            <div className={styles.time_input}>
+                                <label>Tempo estimado</label>
+                                <input
+                                    value={serviceTime}
+                                    onChange={(e) => setServiceTime(e.target.value)}
+                                    type="time"
+                                />
+                            </div>
                         </div>
+
                         <div className={styles.containerButtons}>
                             <button className={styles.btnCancel} onClick={onClose}>Cancelar</button>
-                            <button disabled={disabled} className={styles.btnConfirm}  >Concluído</button>
+                            <button disabled={disabled} className={styles.btnConfirm} onClick={(e) => handleCreateService(e)} >Concluído</button>
                         </div>
                     </form>
                 </div>
